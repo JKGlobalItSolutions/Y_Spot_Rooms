2e4f68e4b032fed2c06b501f33092faa3622ecff
P-JAGADISH <113572661+P-JAGADISH@users.noreply.github.com>
2024-04-08 03:35:57 -0700

    changed 8

---
+++ stays/script/change.js
@@ -0,0 +1,20 @@
+{/* <script>
+    // Function to handle form submission
+    function handleSubmit() {
+        // Get the values entered by the user
+        var destination = document.getElementById("destination").value;
+        var adults = document.getElementsByName("adults")[0].value;
+        var checkin = document.getElementById("checkin").value;
+        var checkout = document.getElementById("checkout").value;
+
+        // Construct the URL with parameters
+        var url = "best-stays.html?";
+        url += "destination=" + encodeURIComponent(destination);
+        url += "&adults=" + encodeURIComponent(adults);
+        url += "&checkin=" + encodeURIComponent(checkin);
+        url += "&checkout=" + encodeURIComponent(checkout);
+
+        // Redirect to the stays page with the parameters
+        window.location.href = url;
+    }
+</script> */}
